6016b00b170e3e78be30cf965992a73adf9118be
David Mohr <david.mohr@ardexa.com>
2016-05-07 21:09:22 +1000

    feat: 8 nodes

--- multi-node/vagrant/config.rb
+++ multi-node/vagrant/config.rb
@@ -3,7 +3,7 @@ $update_channel="beta"
 $controller_count=1
 $controller_vm_memory=1024
 
-$worker_count=4
+$worker_count=8
 $worker_vm_memory=2048
 
 $etcd_count=1
--- multi-node/vagrant/load_internal.sh
+++ multi-node/vagrant/load_internal.sh
@@ -4,6 +4,6 @@ if [[ "$#" -ne 1 ]]; then
     echo "Usage: $0 <url_of_ca_chain>"
 fi
 
-for i in w1 w2 w3 w4; do
+for i in w1 w2 w3 w4 w5 w6 w7 w8; do
     vagrant ssh $i -- 'cd /etc/ssl/certs && sudo wget --no-check-certificate "'$1'" && sudo update-ca-certificates | grep ardexa && sudo systemctl restart docker'
 done
