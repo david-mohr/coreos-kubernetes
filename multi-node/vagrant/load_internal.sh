b41ca5ed8da8ddabd04de153252c93b6fc2bbea9
David Mohr <david.mohr@ardexa.com>
2016-08-29 11:32:13 +1000

    feat: remove old docker cert before downloading the new one

--- multi-node/vagrant/load_internal.sh
+++ multi-node/vagrant/load_internal.sh
@@ -5,5 +5,5 @@ if [[ "$#" -ne 1 ]]; then
 fi
 
 for i in w1 w2 w3 w4 w5 w6 w7 w8 w9; do
-    vagrant ssh $i -- 'cd /etc/ssl/certs && sudo wget --no-check-certificate "'$1'" && sudo update-ca-certificates | grep ardexa && sudo systemctl restart docker'
+    vagrant ssh $i -- 'cd /etc/ssl/certs && sudo rm -f $(basename "'$1'") && sudo wget --no-check-certificate "'$1'" && sudo update-ca-certificates | grep ardexa && sudo systemctl restart docker'
 done
